99ec62a2f7a6b70c60b533625485f8e502021563
Saicharan67 <mahankalisaicharan@gmail.com>
2021-05-11 11:09:07 +0530

    Update ComaritiveSorting.py

--- Sorting/ComaritiveSorting.py
+++ Sorting/ComaritiveSorting.py
@@ -1,27 +1,31 @@
-#Comparators are used to compare two objects. In this challenge, we'll create a comparator and use
-#it to sort an array. The Player class is provided in the editor below. It has two fields:
+# Comparators are used to compare two objects. In this challenge, we'll create a comparator and use
+# it to sort an array. The Player class is provided in the editor below. It has two fields:
 
-# hackerrank question 
+# hackerrank question
 
 from functools import cmp_to_key
+
+
 class Player:
     def __init__(self, name, score):
-        self.name=name
-        self.score=score
+        self.name = name
+        self.score = score
+
     def __repr__(self):
-        
-        return [self.name,self.score]
-        
+
+        return [self.name, self.score]
+
     def comparator(a, b):
-        if a.score>b.score:
-            return -1 
-        elif a.score<b.score:
+        if a.score > b.score:
+            return -1
+        elif a.score < b.score:
             return 1
         else:
-            if a.name>b.name:
+            if a.name > b.name:
                 return 1
             else:
                 return -1
+
 
 n = int(input())
 data = []
@@ -30,7 +34,7 @@ for i in range(n):
     score = int(score)
     player = Player(name, score)
     data.append(player)
-    
+
 data = sorted(data, key=cmp_to_key(Player.comparator))
 for i in data:
     print(i.name, i.score)
