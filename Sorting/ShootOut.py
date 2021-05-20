50611d07f3351b9525b6bd30b218ad0ac11a691a
Saicharan67 <mahankalisaicharan@gmail.com>
2021-05-20 20:41:21 +0530

    Create ShootOut.py

---
+++ Sorting/ShootOut.py
@@ -0,0 +1,13 @@
+def new_func():
+    t = int(input())
+    from math import ceil
+    for _ in range(t):
+        n = int(input())
+        a = 0
+        b = 0
+        s = list(map(int, input().split()))
+        for i in range(len(s)):
+            print(s)
+
+
+new_func()
