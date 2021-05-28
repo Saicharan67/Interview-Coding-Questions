46c2b67c10597a446f751755b09cd2e20f45ab74
Saicharan67 <mahankalisaicharan@gmail.com>
2021-05-28 19:04:42 +0530

    icpc practised

---
+++ BST/ShortestPath.py
@@ -0,0 +1,25 @@
+from icecream import ic
+
+
+def shortestPath(x, y):
+    v1 = []
+    v2 = []
+
+    while x:
+        v1.append(x)
+        x = x//2
+    v1.reverse()
+
+    while y:
+        v2.append(y)
+        y = y//2
+    v2.reverse()
+
+    for i in range(len(v1)-1, -1, -1):
+
+        if v1[i] in v2:
+            x = v2.index(v1[i])
+            return len(v1)-1-i + len(v2)-1-x
+
+
+print(shortestPath(8, 12))
