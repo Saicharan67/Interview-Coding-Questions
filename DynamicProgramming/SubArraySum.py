7403d4b7eec0de1bf97b42d1b81a5a68e9a64514
Saicharan67 <mahankalisaicharan@gmail.com>
2021-06-05 20:52:34 +0530

    Dp sum 1

--- DynamicProgramming/SubArraySum.py
+++ DynamicProgramming/SubArraySum.py
@@ -1,9 +1,9 @@
-dp = [[0]*10]*10
+dp = [[0]*50]*10
 
 # recursive
 
 
-def subarraysumdp(arr, n, k):
+def subarraysumrecurr(arr, n, k):
     if n == 0 or k == 0:
         if k == 0:
             return True
@@ -16,5 +16,23 @@ def subarraysumdp(arr, n, k):
         else:
             return subarraysumdp(arr, n-1, k-arr[n-1]) | subarraysumdp(arr, n-1, k)
 
+# Dp approch
 
-print(subarraysumdp([2, 3, 7, 8, 10], 5, 19))
+
+def subarraysumdp(arr, n, k):
+    for i in range(n+1):
+        dp[i][0] = True
+
+    for j in range(1, k+1):
+        dp[0][j] = False
+
+    for i in range(1, n+1):
+        for j in range(1, k+1):
+            dp[i][j] = dp[i-1][j]
+            if arr[i-1] <= j:
+                dp[i][j] |= dp[i-1][j-arr[i-1]]
+
+    return dp[n][k]
+
+
+print(subarraysumdp([2, 3, 7, 8, 10], 5, 23))
