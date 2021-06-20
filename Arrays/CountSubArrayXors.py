c20c729e01211751cb5de1923c8f4c4bc724b5ac
Saicharan67 <mahankalisaicharan@gmail.com>
2021-06-20 17:48:50 +0530

    Day 27

---
+++ Arrays/CountSubArrayXors.py
@@ -0,0 +1,13 @@
+"""
+
+Given an array of integers arr[] and a number m, count the number of subarrays having XOR of their elements as m.
+Examples: 
+
+Input : arr[] = {4, 2, 2, 6, 4}, m = 6
+Output : 4
+Explanation : The subarrays having XOR of 
+              their elements as 6 are {4, 2}, 
+              {4, 2, 2, 6, 4}, {2, 2, 6},
+               and {6}
+
+"""
