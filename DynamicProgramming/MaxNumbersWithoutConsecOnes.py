ae84b8a4b3f28777ac86c89a131245c3090efe18
Saicharan67 <mahankalisaicharan@gmail.com>
2021-07-25 19:14:27 +0530

    Day 56

---
+++ DynamicProgramming/MaxNumbersWithoutConsecOnes.py
@@ -0,0 +1,33 @@
+'''
+Given a positive integer n, return the number of the integers in the range [0, n] whose binary representations do not contain consecutive ones.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: 5
+Explanation:
+Here are the non-negative integers <= 5 with their corresponding binary representations:
+0 : 0
+1 : 1
+2 : 10
+3 : 11
+4 : 100
+5 : 101
+Among them, only integer 3 disobeys the rule (two consecutive ones) and the other 5 satisfy the rule. 
+'''
+
+
+class Solution:
+    def findIntegers(self, num: int) -> int:
+        x, y = 1, 2
+        res = 0
+        num += 1
+        while num:
+            if num & 1 and num & 2:
+                res = 0
+            res += x * (num & 1)
+            num >>= 1
+            x, y = y, x + y
+        return res
--- Graphs/TopoSort.py
+++ Graphs/TopoSort.py
@@ -4,17 +4,17 @@ from GraphRepresentation import AdjList
 
 def DFSTopo(n, m):
     adjList = AdjList(n, m)
-    visited = [False]*(n+1)
+    visited = [False]*(n)
     dfs = []
 
     def DFS(x):
         visited[x] = True
-        
+
         for y in adjList[x]:
             if not visited[y]:
                 DFS(y)
         dfs.append(x)
-    for i in range(1, n+1):
+    for i in range(n):
         if not visited[i]:
             DFS(i)
     dfs[:] = dfs[::-1]
