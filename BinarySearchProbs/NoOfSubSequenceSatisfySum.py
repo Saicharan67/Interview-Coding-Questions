107f90fd1aea7c38540ec5ae3356b23175f8f285
Saicharan67 <mahankalisaicharan@gmail.com>
2021-07-31 20:24:41 +0530

    61

---
+++ BinarySearchProbs/NoOfSubSequenceSatisfySum.py
@@ -0,0 +1,39 @@
+'''
+Given an array of integers nums and an integer target.
+
+Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+Input: nums = [3,5,6,7], target = 9
+Output: 4
+Explanation: There are 4 subsequences that satisfy the condition.
+[3] -> Min value + max value <= target (3 + 3 <= 9)
+[3,5] -> (3 + 5 <= 9)
+[3,5,6] -> (3 + 6 <= 9)
+[3,6] -> (3 + 6 <= 9)
+Example 2:
+
+Input: nums = [3,3,6,8], target = 10
+Output: 6
+Explanation: There are 6 subsequences that satisfy the condition. (nums can have repeated numbers).
+[3] , [3] , [3,3], [3,6] , [3,6] , [3,3,6]
+
+'''
+
+
+class Solution:
+    def numSubseq(self, A, target: int) -> int:
+        A.sort()
+        l, r = 0, len(A) - 1
+        res = 0
+        mod = 10**9 + 7
+        while l <= r:
+            if A[l] + A[r] > target:
+                r -= 1
+            else:
+                res += pow(2, r - l, mod)
+                l += 1
+        return res % mod
