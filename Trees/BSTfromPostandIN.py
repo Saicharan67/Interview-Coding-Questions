330e4596128680f99f185692675695c59995e6ae
Saicharan67 <mahankalisaicharan@gmail.com>
2021-08-18 22:08:17 +0530

    Update BSTfromPostandIN.py

--- Trees/BSTfromPostandIN.py
+++ Trees/BSTfromPostandIN.py
@@ -2,8 +2,6 @@
 '''
 Given two integer arrays inorder and postorder where inorder is the inorder traversal of a binary tree and postorder is the postorder traversal of the same tree, construct and return the binary tree.
 
- 
-
 Example 1:
 
 
