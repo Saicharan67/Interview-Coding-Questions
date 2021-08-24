7dddcd85d93ebac239cc4572816f6d8c18f45d8f
Saicharan67 <mahankalisaicharan@gmail.com>
2021-08-24 20:29:28 +0530

    Update KnapSackTopDown.py

--- DynamicProgramming/ZeroOneKnapSack/KnapSackTopDown.py
+++ DynamicProgramming/ZeroOneKnapSack/KnapSackTopDown.py
@@ -1,18 +1,18 @@
-dp = [[0 for i in range(60)] for i in range(10)]
 
 
-def knapSack(wt, val, x, n):
-    for i in range(n+1):
-        for j in range(x+1):
+def knapSack(wt, val, capacity, NumberOfItems):
+    dp = [[0 for i in range(capacity+1)] for i in range(NumberOfItems+1)]
+    for i in range(NumberOfItems+1):
+        for j in range(capacity+1):
             if i == 0 or j == 0:
                 dp[i][j] = 0
-    for i in range(1, n+1):
-        for j in range(1, x+1):
+    for i in range(1, NumberOfItems+1):
+        for j in range(1, capacity+1):
             if wt[i-1] <= j:
                 dp[i][j] = max(val[i-1]+dp[i-1][j-wt[i-1]], dp[i-1][j])
             else:
                 dp[i][j] = dp[i-1][j]
-    return dp[n][x]
+    return dp
 
 
-print(knapSack([10, 20, 30], [60, 100, 120], 50, 3))
+print(knapSack([4, 8, 5, 3, 6], [35, 10, 15, 32, 25], 11, 5))
