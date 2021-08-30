9929a0d0612230d78441a39b62c1b61a62f29883
Saicharan67 <mahankalisaicharan@gmail.com>
2021-08-30 20:15:33 +0530

    Create PatchingArray.py

---
+++ Arrays/PatchingArray.py
@@ -0,0 +1,45 @@
+'''
+Given a sorted integer array nums and an integer n, add/patch elements to the array such that any number in the range [1, n] inclusive can be formed by the sum of some elements in the array.
+
+Return the minimum number of patches required.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3], n = 6
+Output: 1
+Explanation:
+Combinations of nums are [1], [3], [1,3], which form possible sums of: 1, 3, 4.
+Now if we add/patch 2 to nums, the combinations are: [1], [2], [3], [1,3], [2,3], [1,2,3].
+Possible sums are 1, 2, 3, 4, 5, 6, which now covers the range [1, 6].
+So we only need 1 patch.
+Example 2:
+
+Input: nums = [1,5,10], n = 20
+Output: 2
+Explanation: The two patches can be [2, 4].
+Example 3:
+
+Input: nums = [1,2,2], n = 5
+Output: 0
+
+'''
+
+
+class Solution:
+    def minPatches(self, nums, n: int) -> int:
+
+        r = 0
+        i = 0
+        cnt = 0
+
+        while r < n:
+            if i < len(nums) and nums[i] <= r+1:
+                r += nums[i]
+                i += 1
+            else:
+                cnt += 1
+                r += r+1
+
+        return cnt
