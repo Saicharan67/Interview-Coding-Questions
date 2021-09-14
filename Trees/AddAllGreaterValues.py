b9116f90570d7d8c9f1b9d430f4f4f5416f4aec2
Saicharan67 <mahankalisaicharan@gmail.com>
2021-09-14 20:39:54 +0530

    Create AddAllGreaterValues.py

---
+++ Trees/AddAllGreaterValues.py
@@ -0,0 +1,42 @@
+'''
+Given a Binary Search Tree (BST), modify it so that all greater values in the given BST are added to every node. For example, consider the following BST.
+
+              50
+           /      \
+         30        70
+        /   \      /  \
+      20    40    60   80 
+
+The above tree should be modified to following 
+
+              260
+           /      \
+         330        150
+        /   \       /  \
+      350   300    210   80
+
+'''
+'''
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+# modify the BST and return its root
+
+
+def modify(root):
+    p = [0]
+
+    def recurr(root):
+        if root:
+            recurr(root.right)
+            root.data += p[-1]
+            p[-1] = root.data
+            recurr(root.left)
+
+    recurr(root)
+    return root
