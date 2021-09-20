00e9b015cd9ab75b2fef74b4f2ef040ee4915a85
Saicharan67 <mahankalisaicharan@gmail.com>
2021-09-20 20:46:06 +0530

    Update DistinctSubsequences.py

--- DynamicProgramming/DistinctSubsequences.py
+++ DynamicProgramming/DistinctSubsequences.py
@@ -1,11 +1,12 @@
 '''
+
+
 Given two strings s and t, return the number of distinct subsequences of s which equals t.
 
 A string's subsequence is a new string formed from the original string by deleting some (can be none) of the characters without disturbing the remaining characters' relative positions. (i.e., "ACE" is a subsequence of "ABCDE" while "AEC" is not).
 
 It is guaranteed the answer fits on a 32-bit signed integer.
 
- 
 
 Example 1:
 
@@ -28,6 +29,7 @@ babgbag
 babgbag
 babgbag
 
+
 '''
 
 
@@ -46,6 +48,3 @@ def numDistinct(s: str, t: str) -> int:
                 dp[i][j] += dp[i-1][j-1]
 
     print(dp[len(t)-1][len(s)-1])
-
-
-print(numDistinct("bccbcdcabadabddbccaddcbabbaaacdba", "bccbbdc"))
