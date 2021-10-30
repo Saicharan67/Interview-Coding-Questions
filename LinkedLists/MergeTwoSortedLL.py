8074c50588d149d931e6a46cb80e278d1a0c5706
Alka107 <78424521+Alka107@users.noreply.github.com>
2021-10-30 18:47:46 +0530

    merge two list

--- LinkedLists/MergeTwoSortedLL.py
+++ LinkedLists/MergeTwoSortedLL.py
@@ -5,30 +5,15 @@ The list should be made by splicing together the nodes of the first two lists.
 
 """
 
-
-class ListNode:
-    def __init__(self, data) -> None:
-        self.val = data
-        self.next = None
-
-
-def mergeTwoLists(ll1: ListNode, ll2: ListNode) -> ListNode:
-    if not ll1 or not ll2:
-        return ll1 or ll2
-    if ll2.val < ll1.val:
-        ll1, ll2 = ll2, ll1
-    x = ll1
-    y = ll2
-    l2 = ListNode(0)
-    tmp = ll1
-    while x and y:
-        if x.val <= y.val:
-            l2.next = x
-            x = x.next
-
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
         else:
-            l2.next = y
-            y = y.next
-        l2 = l2.next
-    l2.next = x or y
-    return tmp
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
